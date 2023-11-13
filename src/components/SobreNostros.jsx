316db36f7addc2583b2f56610d51687301c87ecb
Cristian Ulises Ortega Padron <85147017+Cris2518@users.noreply.github.com>
2023-11-13 00:15:05 -0600

    Archivo eliminado

--- src/components/SobreNostros.jsx
+++
@@ -1,9 +0,0 @@
-import React from "react";
-import { useParams } from "react-router-dom";
-
-const SobreNostros = () => {
-  const { producto } = useParams();
-  // Ahora puedes usar el valor de 'producto' en tu componente
-  alert(producto);
-};
-export default SobreNostros;
